266f339175d9a407691b76c40eabe65e374f6787
remysiri <remysiri@student.arteveldehs.be>
2019-11-18 21:09:13 +0100

    load api

--- src/app/pages/home-page/HomePage.jsx
+++ src/app/pages/home-page/HomePage.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Carousel from '../../components/slider';
 
 const HomePage = () => {
+    const [albums, setAlbums] = useState({});
+
+    async function fetchData() {
+        const res = await fetch('https://cms.unionlao.be/api/albums.json');
+        res
+            .json()
+            .then(res => setAlbums(res.data))
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     return (
-        <Carousel />
+        <Carousel albums={ albums }/>
     )
 }
 
